315d25d0f702d567dcd12a8d5b05ca90b76ec80e
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-10-29 22:40:55 -0600

    Adding footer

---
+++ src/Components/Footer/FooterComponent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './style.css';
+
+const FooterComponent = () => {
+    return (
+        <footer>
+            <div className='footer'>
+                <div className='footer__Container'>
+                    <span>Yacdany Arguedas Mora</span>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default FooterComponent
